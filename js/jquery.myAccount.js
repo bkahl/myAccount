cb050347eb5ec99b026fc796738cc3800146cc7d
Brad Kahl <bkahl1984@gmail.com>
2012-03-07 14:59:05 -0500

    updated with suvajit gupta's date color suggestion, also added documentation/screencast

--- js/jquery.myAccount.js
+++ js/jquery.myAccount.js
@@ -1,8 +1,61 @@
-var d = false, r = false, c = false, p = false;
+var d = false, r = false, c = false, p = false, 
+    date = new Date(), 
+    curr_month = date.getMonth() + 1,
+    curr_date = date.getDate(),
+    curr_year = date.getFullYear();
 
 function isEven(value){
   if (value%2 === 0) return true;
   else return false;
+}
+
+function calcTableSize(rt, rd, rv){
+  var rowWidth = 365, rowTotal = 15, rowDotsWidth = 0;
+  
+  var rowTitle = document.getElementById(rt),
+      rowTitleWidth = (rowTitle.clientWidth+1);
+  $(rowTitle).css('width',rowTitleWidth);
+  rowTotal += rowTitleWidth;
+  
+  var rowValue = document.getElementById(rv),
+      rowValueWidth = rowValue.clientWidth;
+  $(rowValue).css('width',rowValueWidth);
+  rowTotal += rowValueWidth;
+  
+  rowDotsWidth = (rowWidth-rowTotal);    
+  $('#'+rd).css('width',rowDotsWidth); 
+}
+
+function renewalDate(id,v){
+ var m, d, y, difBetweenMonths, difBetweenDays;
+ 
+ m = v.split('/')[0];
+ d = v.split('/')[1];
+ y = v.split('/')[2];
+
+ difBetweenMonths = m-curr_month;
+ difBetweenDays = d - curr_date;
+ 
+ if(y = curr_year){
+   if(difBetweenMonths > 3){
+     $(id).css('color','#719037');
+   } 
+   else if(difBetweenMonths <= 3 && difBetweenMonths >= 2){
+     $(id).css('color','#e48f15');
+   } 
+   else if(difBetweenMonths === 1){
+     $(id).css('color','#c82223');
+   } 
+   else if(difBetweenMonths === 0){
+     if(difBetweenDays > 10) {
+        $(id).css('color','#c82223');
+       }
+     else if(difBetweenDays <= 10) {
+       $(id).css('color','#c82223').css('font-weight','bold');
+     }
+   } 
+ }
+ 
 }
 
 function myAccount(id,data){
@@ -56,6 +109,8 @@ function myAccount(id,data){
     div += "</div>";
     
     $(id).append(div);
+    
+    renewalDate('#row3Value',data.baseProductsAndContactsInfo.renewalDate);
   }else if(data.type === "products"){
     p += true;
     var evenOdd;
@@ -84,7 +139,7 @@ function myAccount(id,data){
             else div += "<tr class='odd' height='20' style='line-height: 20px;'>";
               div += "<td width='165' align='left'style='padding-left: 6px;'>"+o.product+"</td>";
               div += "<td width='100' align='left'>"+o.licenses+"</td>";
-              div += "<td width='100' align='left'>"+o.renewalDate+"</td>";
+              div += "<td width='100' align='left' id='products-renewalDate"+[count]+"'>"+o.renewalDate+"</td>";
             div += "</tr>";
             count++;
           });
@@ -94,6 +149,12 @@ function myAccount(id,data){
     div += "</div>";
     
     $(id).append(div);
+    
+    var z = 0;
+    data.addOnProducts.forEach(function(o){
+      renewalDate('#products-renewalDate'+[z],o.renewalDate);
+      z++;
+    });
   }
   
   if(count > 5) {
@@ -102,27 +163,10 @@ function myAccount(id,data){
   }
 }
 
-function calcTableSize(rt, rd, rv){
-  var rowWidth = 365, rowTotal = 15, rowDotsWidth = 0;
-  
-  var rowTitle = document.getElementById(rt),
-      rowTitleWidth = (rowTitle.clientWidth+1);
-  $(rowTitle).css('width',rowTitleWidth);
-  rowTotal += rowTitleWidth;
-  
-  var rowValue = document.getElementById(rv),
-      rowValueWidth = rowValue.clientWidth;
-  $(rowValue).css('width',rowValueWidth);
-  rowTotal += rowValueWidth;
-  
-  rowDotsWidth = (rowWidth-rowTotal);    
-  $('#'+rd).css('width',rowDotsWidth); 
-}
-
 (function($){
 	$.fn.myAccount = function(options){
 		var id	= $(this), opts;
-
+    
     if(options.type === "director"){
   		$.fn.myAccount.defaults = {
   		  type: 'director',
